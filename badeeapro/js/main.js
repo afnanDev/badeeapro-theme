6f548ccce4c7b7a27f6738d1646aaa16801b309f
afnandev <79251274+afnanDev@users.noreply.github.com>
2022-08-19 16:23:19 +0300

    Delete badeeapro directory

--- badeeapro/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    'use strict';
-		// adjust slider height
-	var winH   = $(window).height(),
-        upperH = $('.upper-bar').innerHeight(),
-		navH   = $('.navbar').innerHeight();
-	$('.slider, .carousel-item').height(winH - (upperH + navH));
-});
